fd46e43c01fdbb8fa86eb8fa8d5eb58ddb14458a
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-11-30 15:20:24 +0800

    Create 10. 正则表达式匹配.cpp

---
+++ 10. 正则表达式匹配.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+bool isMatch(string s, string p) {
+	int m = s.length();
+	int n = p.length();
+	vector<vector<bool>>v(m+1, vector<bool>(n+1));
+	v[0][0] = true;
+	for (int j = 0; 2 * j + 1 <=n; j++)
+	{
+		if (p[2*j+1] == '*')
+		{
+			v[0][2*j+1]=v[0][2*j+2] = true;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+			{
+				v[i][j] = v[i - 1][j - 1];
+			}
+			else if (p[j - 1] == '*')
+			{
+				v[i][j] = v[i][j - 2];
+				if (s[i - 1] == p[j - 2] || p[j-2]=='.')
+				{
+					v[i][j] = v[i][j] || v[i - 1][j];
+				}
+			}
+		}
+	}
+	return v[m][n];
+}
+};
